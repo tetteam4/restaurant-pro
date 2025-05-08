447e0f88441c3b3881de630549a69b45bce4b1b2
Ali-Sina-255 <alisinasultani255@gmail.com>
2025-05-08 10:31:45 +0430

    model is changed

--- Backend/apps/core/urls.py
+++ Backend/apps/core/urls.py
@@ -4,5 +4,11 @@ from . import  views
 
 urlpatterns = [
  path('', views.TypeAPIV.as_view()),
- path('<int:pk>/', views.TypeAPIVIDeleteUpdate.as_view())
+ path('<int:pk>/', views.TypeAPIVIDeleteUpdate.as_view()),
+ path('category/', views.CategoryAPIViewSet.as_view()),
+ path('category/<int:pk>/', views.CategoryAPIVIDeleteUpdate.as_view()),
+ path('attribute/', views.AttributeAPIViewSet.as_view()),
+ path('attribute/<int:pk>/', views.AttributeAPIVIDeleteUpdate.as_view()),
+ path('attribute/value/', views.AttributeValueAPIViewSet.as_view()),
+ path('attribute/value/<int:pk>/', views.AttributeValueAPIVIDeleteUpdate.as_view()),
 ]
